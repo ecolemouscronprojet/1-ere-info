2e50e449d00b286c92b57458ea10c548a1130962
Samuel Lassoie <samuel.lassoie+1@gmail.com>
2021-02-22 12:58:30 +0100

    wip

---
+++ exercices/6.php
@@ -0,0 +1,129 @@
+<?php
+$users = [
+    ['firstName' => 'Steven Barnes', 'lastName' => 'Emma Burnett', 'birthday' => '2020-11-01', 'phone' => '05 70 84 92 12', 'email' => 'Cras.dictum@Quisqueimperdieterat.edu'],
+    ['firstName' => 'Harding Pollard', 'lastName' => 'Walker Caldwell', 'birthday' => '2020-05-15', 'phone' => '04 10 59 88 32', 'email' => 'in@liberolacus.ca'],
+    ['firstName' => 'Lara Romero', 'lastName' => 'Thane Stevenson', 'birthday' => '2021-05-02', 'phone' => '04 17 51 42 18', 'email' => 'consectetuer.euismod.est@cursus.ca'],
+    ['firstName' => 'Len Noble', 'lastName' => 'Aurora Johnston', 'birthday' => '2020-05-30', 'phone' => '09 36 13 68 35', 'email' => 'non.feugiat@interdumligula.net'],
+    ['firstName' => 'Fatima Brady', 'lastName' => 'Xavier Clark', 'birthday' => '2021-04-19', 'phone' => '01 36 80 37 56', 'email' => 'ligula.eu.enim@massanon.org'],
+    ['firstName' => 'Steel Hester', 'lastName' => 'Sylvia Buckley', 'birthday' => '2022-02-02', 'phone' => '07 88 69 68 02', 'email' => 'Maecenas.libero.est@Donec.net'],
+    ['firstName' => 'Yeo Rojas', 'lastName' => 'Samuel Quinn', 'birthday' => '2020-09-18', 'phone' => '04 80 35 86 05', 'email' => 'sociis.natoque@sapienAenean.org'],
+    ['firstName' => 'Logan Osborn', 'lastName' => 'Brenda Cook', 'birthday' => '2021-09-12', 'phone' => '08 62 01 91 98', 'email' => 'cursus@eteuismodet.net'],
+    ['firstName' => 'Driscoll Shelton', 'lastName' => 'Cain Wilder', 'birthday' => '2020-07-30', 'phone' => '04 62 18 52 16', 'email' => 'massa.non@aclibero.ca'],
+    ['firstName' => 'Nissim Lowery', 'lastName' => 'Devin Mendoza', 'birthday' => '2021-12-08', 'phone' => '05 16 29 55 39', 'email' => 'euismod.mauris.eu@parturientmontesnascetur.edu'],
+    ['firstName' => 'Vincent Lang', 'lastName' => 'Kato Fry', 'birthday' => '2020-11-30', 'phone' => '07 72 73 43 59', 'email' => 'Proin.ultrices.Duis@sapiengravidanon.com'],
+    ['firstName' => 'Ciara Gentry', 'lastName' => 'Ignatius Livingston', 'birthday' => '2022-02-14', 'phone' => '09 88 03 72 48', 'email' => 'purus@a.org'],
+    ['firstName' => 'Derek Conrad', 'lastName' => 'Liberty Browning', 'birthday' => '2021-02-23', 'phone' => '08 20 44 90 13', 'email' => 'neque.Sed.eget@ridiculusmus.ca'],
+    ['firstName' => 'Chancellor Allen', 'lastName' => 'Sonya Harrell', 'birthday' => '2021-01-09', 'phone' => '05 85 69 32 27', 'email' => 'elit.pretium@eratvolutpatNulla.net'],
+    ['firstName' => 'TaShya Meadows', 'lastName' => 'Hashim Reilly', 'birthday' => '2022-02-02', 'phone' => '04 15 01 81 43', 'email' => 'Nunc.mauris.sapien@pretiumaliquet.com'],
+    ['firstName' => 'Risa Watson', 'lastName' => 'Karen Mcdaniel', 'birthday' => '2020-11-17', 'phone' => '05 79 27 50 72', 'email' => 'ultrices@nullaante.net'],
+    ['firstName' => 'Katelyn Duncan', 'lastName' => 'Cade Rivers', 'birthday' => '2021-01-10', 'phone' => '04 33 04 28 23', 'email' => 'Integer.eu@quis.org'],
+    ['firstName' => 'Raphael Andrews', 'lastName' => 'Cherokee Joseph', 'birthday' => '2021-07-26', 'phone' => '06 89 78 19 90', 'email' => 'elit@sitamet.net'],
+    ['firstName' => 'Ulysses Dunlap', 'lastName' => 'Jolie Stout', 'birthday' => '2020-11-11', 'phone' => '03 88 85 84 62', 'email' => 'Proin.mi@torquentper.ca'],
+    ['firstName' => 'Abigail Ingram', 'lastName' => 'Charissa Espinoza', 'birthday' => '2020-03-16', 'phone' => '01 12 57 47 57', 'email' => 'massa.Suspendisse@ametmetusAliquam.net'],
+    ['firstName' => 'Hayfa Rosa', 'lastName' => 'Kyla Bond', 'birthday' => '2020-12-17', 'phone' => '07 64 69 44 76', 'email' => 'pellentesque.tellus@lobortis.ca'],
+    ['firstName' => 'MacKenzie Mcfarland', 'lastName' => 'Jemima Workman', 'birthday' => '2022-01-07', 'phone' => '04 84 36 34 86', 'email' => 'a@enimNunc.co.uk'],
+    ['firstName' => 'Mary Whitley', 'lastName' => 'Julian Whitehead', 'birthday' => '2020-04-27', 'phone' => '08 92 04 82 42', 'email' => 'sapien@eget.edu'],
+    ['firstName' => 'Lev Moran', 'lastName' => 'Victoria Espinoza', 'birthday' => '2021-02-22', 'phone' => '09 01 92 67 56', 'email' => 'lectus.sit.amet@mattis.net'],
+    ['firstName' => 'Ulysses Haley', 'lastName' => 'Vivian Bush', 'birthday' => '2021-01-05', 'phone' => '06 56 44 91 76', 'email' => 'consectetuer.adipiscing@at.ca'],
+    ['firstName' => 'Desiree Hammond', 'lastName' => 'Anne Woodard', 'birthday' => '2022-02-07', 'phone' => '05 57 85 47 92', 'email' => 'fermentum.fermentum.arcu@urnajusto.ca'],
+    ['firstName' => 'Lisandra Bridges', 'lastName' => 'Carter Christensen', 'birthday' => '2021-06-16', 'phone' => '05 30 33 88 39', 'email' => 'suscipit.nonummy@miacmattis.net'],
+    ['firstName' => 'Burke English', 'lastName' => 'Fiona Collier', 'birthday' => '2021-02-21', 'phone' => '03 30 62 41 57', 'email' => 'enim.Nunc.ut@loremutaliquam.net'],
+    ['firstName' => 'Macon Brady', 'lastName' => 'Jescie Sandoval', 'birthday' => '2021-08-01', 'phone' => '01 83 53 91 73', 'email' => 'dis.parturient.montes@primisinfaucibus.com'],
+    ['firstName' => 'Jayme Kirk', 'lastName' => 'Giacomo Morse', 'birthday' => '2021-09-09', 'phone' => '08 06 12 31 55', 'email' => 'convallis.in@velturpis.org'],
+    ['firstName' => 'Hyatt Pitts', 'lastName' => 'Noelle Ochoa', 'birthday' => '2022-01-29', 'phone' => '02 31 35 16 12', 'email' => 'convallis.dolor.Quisque@vulputateposuere.org'],
+    ['firstName' => 'Yuri Hatfield', 'lastName' => 'Ralph Park', 'birthday' => '2021-03-09', 'phone' => '08 05 27 29 48', 'email' => 'morbi@adipiscingelitEtiam.co.uk'],
+    ['firstName' => 'Sierra Kirby', 'lastName' => 'Louis Griffith', 'birthday' => '2020-08-08', 'phone' => '02 23 86 80 11', 'email' => 'adipiscing@inhendreritconsectetuer.net'],
+    ['firstName' => 'Audrey Gay', 'lastName' => 'Barclay Zamora', 'birthday' => '2020-12-20', 'phone' => '08 00 41 86 63', 'email' => 'Cras.dolor.dolor@consectetuer.co.uk'],
+    ['firstName' => 'Nevada Patton', 'lastName' => 'Zena Weber', 'birthday' => '2022-01-05', 'phone' => '06 41 90 08 24', 'email' => 'dui@dolorFusce.org'],
+    ['firstName' => 'Priscilla Alford', 'lastName' => 'Jeremy Goff', 'birthday' => '2021-03-25', 'phone' => '02 29 20 60 57', 'email' => 'mollis.Duis.sit@urnaNunc.org'],
+    ['firstName' => 'Zephania Grimes', 'lastName' => 'Leilani Estes', 'birthday' => '2020-12-05', 'phone' => '02 50 31 48 90', 'email' => 'nec.euismod.in@tortorNunccommodo.co.uk'],
+    ['firstName' => 'Hedy Berg', 'lastName' => 'Rachel Cortez', 'birthday' => '2020-08-16', 'phone' => '09 44 31 42 29', 'email' => 'vitae.mauris@sagittis.net'],
+    ['firstName' => 'August Richard', 'lastName' => 'Ocean Spencer', 'birthday' => '2020-05-18', 'phone' => '04 94 52 97 40', 'email' => 'ultrices@convallis.co.uk'],
+    ['firstName' => 'Aiko Fields', 'lastName' => 'Clio Clayton', 'birthday' => '2021-01-11', 'phone' => '05 91 67 02 30', 'email' => 'diam@quispede.co.uk'],
+    ['firstName' => 'Carl Wells', 'lastName' => 'Samantha Drake', 'birthday' => '2020-05-29', 'phone' => '07 05 74 52 29', 'email' => 'Vestibulum.ante.ipsum@Duisgravida.net'],
+    ['firstName' => 'Harlan Bradshaw', 'lastName' => 'Lareina Rocha', 'birthday' => '2021-06-03', 'phone' => '05 96 35 63 23', 'email' => 'Nulla@ornareegestas.co.uk'],
+    ['firstName' => 'Tamara Hensley', 'lastName' => 'Paki Gordon', 'birthday' => '2020-12-14', 'phone' => '02 43 92 87 49', 'email' => 'in.consectetuer@Curabitursed.net'],
+    ['firstName' => 'Roth Kramer', 'lastName' => 'Yvette Bolton', 'birthday' => '2020-09-28', 'phone' => '03 79 15 23 90', 'email' => 'tristique.pellentesque@et.edu'],
+    ['firstName' => 'Chadwick Higgins', 'lastName' => 'Tanya Rivers', 'birthday' => '2021-11-26', 'phone' => '04 36 73 10 19', 'email' => 'Maecenas@tempusloremfringilla.com'],
+    ['firstName' => 'Kendall Avila', 'lastName' => 'Flynn Hammond', 'birthday' => '2020-05-05', 'phone' => '07 76 18 32 67', 'email' => 'amet.consectetuer@nonjustoProin.com'],
+    ['firstName' => 'Maisie William', 'lastName' => 'Denton Merritt', 'birthday' => '2022-01-26', 'phone' => '03 39 89 75 66', 'email' => 'Nunc.pulvinar@euismodindolor.net'],
+    ['firstName' => 'Dacey Gregory', 'lastName' => 'Madonna Bentley', 'birthday' => '2021-07-14', 'phone' => '06 74 70 30 01', 'email' => 'varius.Nam.porttitor@molestie.edu'],
+    ['firstName' => 'Cherokee Pope', 'lastName' => 'Richard Alvarez', 'birthday' => '2021-06-25', 'phone' => '01 97 95 14 98', 'email' => 'purus.Nullam.scelerisque@convallis.co.uk'],
+    ['firstName' => 'Addison Holder', 'lastName' => 'Chancellor Morgan', 'birthday' => '2020-05-22', 'phone' => '05 91 86 73 88', 'email' => 'Pellentesque.ultricies.dignissim@eteuismod.org'],
+    ['firstName' => 'Octavius Joyce', 'lastName' => 'Baxter Flores', 'birthday' => '2020-05-02', 'phone' => '08 03 43 02 74', 'email' => 'ut@quamdignissimpharetra.com'],
+    ['firstName' => 'Carson Bowman', 'lastName' => 'Carlos Whitney', 'birthday' => '2020-09-28', 'phone' => '02 66 22 59 22', 'email' => 'Aenean@sodalespurus.org'],
+    ['firstName' => 'MacKenzie Acevedo', 'lastName' => 'Beverly Franklin', 'birthday' => '2022-02-04', 'phone' => '01 73 98 12 06', 'email' => 'arcu.Vestibulum@montesnasceturridiculus.org'],
+    ['firstName' => 'Amelia Holloway', 'lastName' => 'Evangeline Maddox', 'birthday' => '2021-02-14', 'phone' => '07 02 58 92 21', 'email' => 'Suspendisse.tristique.neque@Namporttitor.net'],
+    ['firstName' => 'Colby Houston', 'lastName' => 'Cameran Sharpe', 'birthday' => '2020-05-30', 'phone' => '02 71 76 86 96', 'email' => 'in@egetipsumSuspendisse.org'],
+    ['firstName' => 'Risa Daugherty', 'lastName' => 'Garrison Wynn', 'birthday' => '2020-09-08', 'phone' => '04 85 47 60 44', 'email' => 'et.netus@laciniaorci.com'],
+    ['firstName' => 'Ali Dyer', 'lastName' => 'Darrel Bender', 'birthday' => '2020-03-13', 'phone' => '08 90 88 69 89', 'email' => 'Integer.urna@Sedet.org'],
+    ['firstName' => 'Ignacia Calderon', 'lastName' => 'Kelsey Howe', 'birthday' => '2021-11-06', 'phone' => '01 85 71 16 79', 'email' => 'risus.Morbi.metus@eu.com'],
+    ['firstName' => 'Gregory Cooke', 'lastName' => 'Merrill Drake', 'birthday' => '2021-02-02', 'phone' => '04 05 83 69 80', 'email' => 'penatibus.et@NullainterdumCurabitur.edu'],
+    ['firstName' => 'Zena Walter', 'lastName' => 'Rudyard Mayer', 'birthday' => '2020-10-05', 'phone' => '05 40 97 89 59', 'email' => 'amet.risus@sitamet.ca'],
+    ['firstName' => 'Lewis Brooks', 'lastName' => 'Wesley Willis', 'birthday' => '2021-09-05', 'phone' => '08 68 56 30 36', 'email' => 'feugiat.nec@loremsit.org'],
+    ['firstName' => 'Griffith Craig', 'lastName' => 'Brenden Adkins', 'birthday' => '2022-01-12', 'phone' => '02 20 27 38 51', 'email' => 'placerat@egestasa.com'],
+    ['firstName' => 'Angelica Wells', 'lastName' => 'Hilel Stone', 'birthday' => '2021-04-23', 'phone' => '02 10 59 26 29', 'email' => 'pellentesque.tellus@volutpatornarefacilisis.net'],
+    ['firstName' => 'Nissim Wade', 'lastName' => 'Fulton Lynn', 'birthday' => '2021-09-07', 'phone' => '01 10 05 22 16', 'email' => 'fermentum.convallis@eteros.ca'],
+    ['firstName' => 'Rylee Fletcher', 'lastName' => 'Chiquita Oneill', 'birthday' => '2021-05-11', 'phone' => '02 94 14 16 30', 'email' => 'ultrices@eu.co.uk'],
+    ['firstName' => 'Patricia Patterson', 'lastName' => 'Hyacinth Hernandez', 'birthday' => '2022-01-23', 'phone' => '05 65 16 02 01', 'email' => 'consectetuer@duiFuscealiquam.ca'],
+    ['firstName' => 'Gregory Rice', 'lastName' => 'Olympia Dorsey', 'birthday' => '2020-08-12', 'phone' => '05 38 93 62 64', 'email' => 'enim@dignissimMaecenas.ca'],
+    ['firstName' => 'Abdul Adams', 'lastName' => 'Maite Clements', 'birthday' => '2021-05-31', 'phone' => '08 72 67 64 17', 'email' => 'neque.et.nunc@nuncsed.co.uk'],
+    ['firstName' => 'Geraldine Henderson', 'lastName' => 'Demetrius Rogers', 'birthday' => '2021-10-13', 'phone' => '05 87 96 37 37', 'email' => 'eu.tellus@Nullatempor.org'],
+    ['firstName' => 'Aladdin Odonnell', 'lastName' => 'Alan Mcguire', 'birthday' => '2020-04-24', 'phone' => '02 24 61 49 91', 'email' => 'magna.tellus@ornare.edu'],
+    ['firstName' => 'Leilani Kinney', 'lastName' => 'Ryan Blackburn', 'birthday' => '2020-10-09', 'phone' => '05 50 89 70 76', 'email' => 'ullamcorper.magna@egetlacus.ca'],
+    ['firstName' => 'Odysseus Craft', 'lastName' => 'Desiree Mcbride', 'birthday' => '2021-12-07', 'phone' => '09 33 41 18 75', 'email' => 'elit.pede.malesuada@estMauriseu.edu'],
+    ['firstName' => 'Danielle Mitchell', 'lastName' => 'Emmanuel Walls', 'birthday' => '2021-11-10', 'phone' => '06 80 08 77 17', 'email' => 'erat.eget@Morbi.net'],
+    ['firstName' => 'Adam Holt', 'lastName' => 'Amelia Farmer', 'birthday' => '2020-07-06', 'phone' => '03 64 31 73 35', 'email' => 'pellentesque@ipsumleo.net'],
+    ['firstName' => 'Phillip Joyner', 'lastName' => 'Jack Rivas', 'birthday' => '2021-08-17', 'phone' => '02 77 91 16 73', 'email' => 'Suspendisse@Phasellusdapibus.com'],
+    ['firstName' => 'Logan Quinn', 'lastName' => 'Cooper Carney', 'birthday' => '2020-02-28', 'phone' => '06 92 40 17 82', 'email' => 'gravida.molestie@elit.com'],
+    ['firstName' => 'Jacqueline Tyler', 'lastName' => 'Clementine Kramer', 'birthday' => '2020-12-05', 'phone' => '07 12 07 47 69', 'email' => 'aliquam.arcu.Aliquam@Fuscediam.org'],
+    ['firstName' => 'Sandra Oneill', 'lastName' => 'Nehru Adams', 'birthday' => '2021-02-24', 'phone' => '04 88 28 85 14', 'email' => 'fermentum.vel@Fuscemi.co.uk'],
+    ['firstName' => 'Roary Roman', 'lastName' => 'Damian Hester', 'birthday' => '2020-04-21', 'phone' => '04 55 13 57 24', 'email' => 'felis@eratvolutpat.org'],
+    ['firstName' => 'Lael Mason', 'lastName' => 'Jada Olson', 'birthday' => '2021-08-28', 'phone' => '04 15 77 83 29', 'email' => 'lobortis.tellus@antedictum.org'],
+    ['firstName' => 'Michelle Walter', 'lastName' => 'Urielle Witt', 'birthday' => '2021-03-08', 'phone' => '09 09 38 45 38', 'email' => 'neque.Nullam@sem.org'],
+    ['firstName' => 'Honorato Barlow', 'lastName' => 'Quincy Sellers', 'birthday' => '2021-07-20', 'phone' => '04 09 17 84 85', 'email' => 'vel.faucibus.id@parturientmontes.co.uk'],
+    ['firstName' => 'Jayme Morse', 'lastName' => 'Allen Mcdowell', 'birthday' => '2021-01-07', 'phone' => '02 70 93 93 79', 'email' => 'euismod.ac@Etiam.com'],
+    ['firstName' => 'April Yates', 'lastName' => 'Channing Cohen', 'birthday' => '2021-03-09', 'phone' => '04 88 48 34 82', 'email' => 'neque.In.ornare@lobortistellusjusto.org'],
+    ['firstName' => 'Nash Sharpe', 'lastName' => 'Orson Kim', 'birthday' => '2020-06-25', 'phone' => '02 09 00 19 29', 'email' => 'est@sagittissemperNam.com'],
+    ['firstName' => 'Caesar Brady', 'lastName' => 'Yasir Swanson', 'birthday' => '2020-05-18', 'phone' => '06 38 53 86 50', 'email' => 'pede.Cras.vulputate@metussitamet.net'],
+    ['firstName' => 'Miriam Nash', 'lastName' => 'Alan Rich', 'birthday' => '2021-01-20', 'phone' => '09 79 69 84 41', 'email' => 'velit.dui.semper@In.edu'],
+    ['firstName' => 'Devin Quinn', 'lastName' => 'Quemby Mcfadden', 'birthday' => '2021-07-20', 'phone' => '08 72 01 84 20', 'email' => 'consequat.nec.mollis@enimnisl.com'],
+    ['firstName' => 'Graham Crosby', 'lastName' => 'Sonya Mendoza', 'birthday' => '2021-01-26', 'phone' => '09 31 78 31 97', 'email' => 'a.ultricies@erat.org'],
+    ['firstName' => 'Nayda Parks', 'lastName' => 'Dean Stone', 'birthday' => '2020-05-07', 'phone' => '09 60 82 69 54', 'email' => 'ut@Proinultrices.org'],
+    ['firstName' => 'Justin Sharpe', 'lastName' => 'Dane Graves', 'birthday' => '2021-04-22', 'phone' => '01 85 52 46 13', 'email' => 'in.consequat@vitae.net'],
+    ['firstName' => 'Mia Anthony', 'lastName' => 'Justine Rowland', 'birthday' => '2020-07-18', 'phone' => '05 21 02 21 58', 'email' => 'in@velesttempor.org'],
+    ['firstName' => 'Zachary Jacobs', 'lastName' => 'Octavius Nunez', 'birthday' => '2021-03-11', 'phone' => '08 93 31 61 99', 'email' => 'vitae@sed.org'],
+    ['firstName' => 'Laura Barlow', 'lastName' => 'Giacomo Bullock', 'birthday' => '2021-10-16', 'phone' => '09 26 41 54 09', 'email' => 'arcu@sed.net'],
+    ['firstName' => 'Cassidy Cash', 'lastName' => 'Alvin Prince', 'birthday' => '2020-05-09', 'phone' => '05 71 35 39 65', 'email' => 'Fusce.diam.nunc@consectetuer.net'],
+    ['firstName' => 'Grant Love', 'lastName' => 'Hannah Alvarez', 'birthday' => '2020-03-02', 'phone' => '03 01 95 16 24', 'email' => 'eget.ipsum.Suspendisse@diamdictumsapien.org'],
+    ['firstName' => 'Zane Hays', 'lastName' => 'Hop Merrill', 'birthday' => '2021-09-18', 'phone' => '07 92 47 16 42', 'email' => 'Duis.gravida.Praesent@ipsum.org'],
+    ['firstName' => 'Cade Ramsey', 'lastName' => 'Ocean Solis', 'birthday' => '2021-09-09', 'phone' => '04 94 13 92 83', 'email' => 'nunc.In.at@duinectempus.edu'],
+    ['firstName' => 'Randall Castro', 'lastName' => 'Erich Mercado', 'birthday' => '2020-05-15', 'phone' => '09 23 63 69 29', 'email' => 'pharetra@euismodenimEtiam.org'],
+    ['firstName' => 'Geraldine Bowen', 'lastName' => 'Blaze Manning', 'birthday' => '2021-08-07', 'phone' => '02 93 07 31 69', 'email' => 'dictum.eu.placerat@fringilla.ca'],
+];
+
+
+echo '<h1>Exercice n°1</h1><br>';
+
+if(isset($users[3])) {
+    echo $users[3]['firstName'] .' ' .$users[3]['lastName'].'<br>';
+}
+
+
+echo '<h1>Exercice n°2</h1><br>';
+
+echo $users[14]['firstName'] .' ' . $users[9]['lastName'] .' ' . $users[7]['birthday'] .'<br>';
+
+
+echo '<h1>Exercice n°3</h1><br>';
+
+//echo $users[2]['birthday'].'<br>';
+try{
+    $birthday = new DateTime($users[2]['birthday']);
+    echo $birthday->format('d/m/Y').'<br>';
+} catch(Exception $e) {
+    echo 'Date non valide';
+}
+
+
+//var_dump($birthday);
